dfcd4bcedf720a341796afaadf9262bb64efe0dc
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2024-11-15 16:33:25 +0000

    absolute mess

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,13 @@
 
     var apiPrefix = "/api/v1";
     var token = "";
-    var tokenInterval = null;
     let autoScroll: boolean;
     let isScrolling = false;
     let songValue = "";
     let currentPage = 1;
 
-    $: subtitles = [];
-    $: info = [];
+    let subtitles: any = []; // an array of subtitles (i forgot what is inside)
+    let info: object[] = [];
     let then = 0;
 
     async function searchSong(name: string, page: number = 1) {
@@ -50,7 +49,7 @@
             lyricsCont.forEach((el, i) => {
                 // console.log(diff);
                 var diff = (new Date()).getTime() - then;
-                if (parseInt(el.getAttribute("data-timestamp")) > (diff - 10) && parseInt(el.getAttribute("data-timestamp")) < (diff + 10)) {
+                if (parseInt(el.getAttribute("data-timestamp") || "0") > (diff - 10) && parseInt(el.getAttribute("data-timestamp") || "0") < (diff + 10)) {
                     (document.querySelectorAll(".active")).forEach((a, b) => {
                         document.querySelectorAll(".active")[b].classList.remove("active");
                     });
@@ -59,7 +58,7 @@
                         clearInterval(interv);
                     }
                     if (autoScroll && !isScrolling) {
-                        el.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest"});
+                        el.scrollIntoView({ behavior: "smooth", block: "center", inline: "center"});
                     }
                 }
             });
@@ -106,25 +105,16 @@
 
     onMount(() => {
         checkForScroll();
-        (document.getElementById("submit") as HTMLInputElement)!.disabled = true;
+        (document.getElementById("submit") as HTMLInputElement)!.disabled = true; // disable the search button
         parseToken(); // parse for musixmatch token
         document.getElementById("artistForm")?.addEventListener("submit", (e) => {
             e.preventDefault();
             currentPage = 1;
             searchSong((<HTMLInputElement>document.getElementById("song"))?.value).then((data) => {
-                // console.log(data);
-                /* var info = data["message"]["body"]["macro_calls"]["track.lyrics.get"]["message"]["body"]["lyrics"];
-                var instrumental = false ? info.instrumental === 0 : true;
-                var explicit = false ? info.explicit === 0 : true;
-
-                var instrumentalHTML = document.getElementById("instrumental");
-                var explicitHTML = document.getElementById("explicit");
-
+                /*
                 instrumentalHTML!.innerHTML = `<b>Instrumental</b>: ${instrumental.toString()}<br />`;
                 explicitHTML!.innerHTML = `<b>Explicit</b>: ${explicit.toString()}`;
-
-                var lyrics = (info.lyrics_body).replace(/(?:\r\n|\r|\n)/g, '<br>');
-                document.getElementById("lyrics")!.innerHTML = lyrics; */
+                */
                 if (data.message.header.status_code !== 200) {
                     info = [{ track_name: "ERROR" }];
                 }
@@ -141,7 +131,7 @@
 </svelte:head>
 
 <form id="artistForm" method="post">
-    <div>
+    <div style="padding: .5em 0;">
         <label for="song"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path opacity="1" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></label>
         <input type="text" bind:value={songValue} autocorrect="off" autocapitalize="off" name="song" required id="song" placeholder="Song, lyrics, artist..." spellcheck="false">
     </div>
@@ -150,17 +140,19 @@
 
 <br />
 
-
+<!-- TODO do this -->
 <span id="instrumental"></span>
 <span id="explicit"></span>
 
 <i>Not what you're looking for? Add the artist into your search</i><br>
 <p id="error-musixmatch" style="color: #f00; display: none;">Sorry, Musixmatch is currently blocking your requests. Please wait up to 5 minutes - the issue will resolve itself.</p>
 <ul id="searchResults">
+    <!-- TODO: make this a carousel of vertical cards LOL -->
     {#each info as thing, i}
         <li>
             <button class="searchButtons" on:click={() => parseLyrics(thing.track.track_id)}>
-                <img src={thing.track.album_coverart_100x100} alt="Cover image of {thing.track.track_name}" width="50" height="50">
+                <!-- this replace job is not my finest work. -->
+                <img src={thing.track.album_coverart_100x100.replace("http", "https").replace("httpss", "https")} alt="Cover image of {thing.track.track_name}" width="50" height="50">
                 <div>
                     <b>{thing.track.track_name}</b>
                     <br />
@@ -174,7 +166,7 @@
         <!-- wonderful code here, not a pain to see -->
         <button class="searchButtons" on:click={() => { if (currentPage > 1 && info.length > 0) { searchSong(songValue, currentPage - 1).then((data) => { info = (data.message.header.status_code !== 200) ? [{ track_name: "ERROR" }] : data.message.body.track_list; }); currentPage -= 1; } }}><svg height="16" width="12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg></button>
         <span>{currentPage}</span>
-        <button class="searchButtons" on:click={() => { if (info.length > 0) { searchSong(songValue, currentPage + 1).then((data) => { info = (data.message.header.status_code !== 200) ? [{ track_name: "ERROR" }] : data.message.body.track_list; });; currentPage += 1; }}}><svg height="16" width="12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg></button>
+        <button class="searchButtons" on:click={() => { if (info.length > 0) { searchSong(songValue, currentPage + 1).then((data) => { info = (data.message.header.status_code !== 200) ? [{ track_name: "ERROR" }] : data.message.body.track_list; }); currentPage += 1; }}}><svg height="16" width="12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg></button>
     </li>
 </ul>
 
@@ -188,14 +180,21 @@
     <i>Press Play Lyrics before trying to click lyrics</i>
     <i>Click on a lyric to skip to it</i><br>
     {#each subtitles as sub, i}
-        <button on:click={() => { then = (new Date()).getTime() - ((sub.time.hundredths * 10) + (sub.time.minutes * 60 * 1000) + (sub.time.seconds * 1000))}} data-timestamp={(sub.time.hundredths * 10) + (sub.time.minutes * 60 * 1000) + (sub.time.seconds * 1000)}>{sub.text}</button><br />
+        <button on:click={() => { then = (new Date()).getTime() - ((sub.time.hundredths * 10) + (sub.time.minutes * 60 * 1000) + (sub.time.seconds * 1000))}} data-timestamp={(sub.time.hundredths * 10) + (sub.time.minutes * 60 * 1000) + (sub.time.seconds * 1000)}>
+            {#if sub.text === ""}
+                ♪
+            {:else}
+                {sub.text}
+            {/if}
+        </button>
+        <br />
     {/each}
 </p>
 
 <style lang="scss">
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
     * {
-        font-family: 'Outfit', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif!important;
+        font-family: 'Inter', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif!important;
     }
     button {
         cursor: pointer;
@@ -254,6 +253,7 @@
             color: white;
             background-color: #0873ff;
             cursor: pointer;
+            transition: .1s;
             &:hover {
                 filter: brightness(0.9);
             }
@@ -290,7 +290,7 @@
         margin: 0 .5rem;
         overflow: hidden;
     }
-    #playButton, #scrollButton {
+    #playButton {
         padding: .5rem;
         margin-left: .5rem;
         // aspect-ratio: 1 / 1;
@@ -318,7 +318,8 @@
             background-color: #222;
             color: white;
         }
-        #playButton, .searchButtons, #lyrics button, #scrollButton {
+
+        #playButton, .searchButtons, #lyrics button {
             color: white;
         }
         svg {
